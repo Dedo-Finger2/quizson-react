0337fc03fd1e27b9400b1db893bf855346fa46c6
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-08 20:20:39 -0300

    Uses About page + tries to apply responsiviness

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import Hero from "./components/Hero";
 import Features from "./components/Features";
 
 import "./App.css";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <div className="px-28 flex flex-col gap-12">
+      <div className="xl:200 lg:px-140 md:px-20 items-center justify-center flex flex-col gap-12">
         <Hero />
         <Features />
+        <About />
       </div>
     </div>
   );
