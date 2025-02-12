7c6f0b7928a0befd0f96e9d39c83d7ac31a308bd
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-12 14:20:52 -0300

    Adds the same links from Navbar into Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,23 @@
 import { CiLinkedin } from "react-icons/ci";
 import { FiGithub } from "react-icons/fi";
 import { FaInstagram } from "react-icons/fa";
+import { Link } from "react-router";
 
 function Footer() {
   return (
     <div className="w-full items-center text-orange-100/70 flex flex-col gap-8">
       <span>A. Neto - antonioimportant@gmail.com</span>
+      <div className="flex gap-8">
+        <span>
+          <Link to="/">Home</Link>
+        </span>
+        <span>
+          <a href="#Features">Features</a>
+        </span>
+        <span>
+          <a href="#About">About</a>
+        </span>
+      </div>
       <div className="flex gap-4">
         <CiLinkedin
           className="transition-colors ease-in-out hover:text-orange-400 hover:cursor-pointer"
