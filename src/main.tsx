c53af41da39eabed2c344ddb679939c146ae1dc2
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-12 09:07:57 -0300

    Adds QuizPage

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import "./index.css";
 import HomePage from "./pages/HomePage";
 import { BrowserRouter, Route, Routes } from "react-router";
 import UploadFilePage from "./pages/UploadFile";
+import QuizPage from "./pages/QuizPage";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/upload-file" element={<UploadFilePage />} />
+      <Route path="/quiz" element={<QuizPage />} />
     </Routes>
   </BrowserRouter>
 );
---
+++ src/pages/QuizPage.tsx
@@ -0,0 +1,65 @@
+import Navbar from "../components/Navbar";
+import Alternative from "../components/Quiz/Alternative";
+import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
+
+function QuizPage() {
+  return (
+    <div className="h-screen flex flex-col">
+      <Navbar />
+      <div className="flex flex-col items-center mt-16">
+        <div className="text-orange-50 flex flex-col gap-8 h-full w-250">
+          {/* Header */}
+          <div className="flex flex-col gap-4 justify-center select-none">
+            <span className="font-semibold text-lg">Question 1/6</span>
+            <div className="relative">
+              <div
+                style={{ width: "20%" }}
+                className="transition-all ease-in-out h-2 rounded-full bg-orange-400 absolute"
+              ></div>
+              <div className="w-full h-2 rounded-full bg-orange-300/30"></div>
+            </div>
+            <span className="text-sm text-orange-100/80">
+              <span>04:00</span> left
+            </span>
+          </div>
+          {/* Question */}
+          <div className="flex gap-4 select-none">
+            <div className="p-4 bg-orange-300/20 rounded-xl flex items-center justify-center">
+              <span className="border-2 font-bold border-orange-50 rounded-full px-2 flex items-center justify-center">
+                5
+              </span>
+            </div>
+            <div className="flex flex-col gap-0.5">
+              <h1 className="font-bold text-xl">5. Paris</h1>
+              <span className="font-medium text-sm text-orange-100/80">
+                What is the capital of France?
+              </span>
+            </div>
+          </div>
+          {/* Alternatives */}
+          <div className="grid grid-cols-2 gap-4">
+            <Alternative content="Alternative 01" id={1} />
+            <Alternative content="Alternative 02" id={2} />
+            <Alternative content="Alternative 03" id={3} />
+            <Alternative content="Alternative 04" id={4} />
+            <Alternative content="Alternative 05" id={5} />
+          </div>
+          {/* Question controls */}
+          <div className="flex justify-between select-none">
+            <button className="flex flex-row gap-2 items-center bg-orange-400/20 text-neutral-400 py-1.5 px-6 rounded-sm hover:cursor-not-allowed font-semibold text-base">
+              <FaArrowLeft />
+              Previous
+            </button>
+            <button className="flex flex-row gap-2 items-center bg-orange-400 py-1.5 px-6 rounded-sm hover:bg-orange-500 hover:cursor-pointer font-semibold text-base">
+              Next
+              <FaArrowRight />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default QuizPage;
