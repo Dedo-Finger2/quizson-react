d3c3008da4190ce0974448c0d53d5c5ed36e87dc
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-12 20:57:04 -0300

    Removes Navbar Links (saved to obsidian for later)

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,4 @@
 import { MdQuiz } from "react-icons/md";
-import { FaFileImport } from "react-icons/fa";
-import { Link } from "react-router";
 
 function Navbar() {
   return (
@@ -11,29 +9,6 @@ function Navbar() {
           <span className="text-orange-400">Quiz</span>
           son
         </span>
-      </div>
-      <div className="flex gap-12 items-center">
-        <div className="flex gap-8">
-          <span className="transition-colors ease-in-out font-semibold cursor-pointer hover:text-orange-400">
-            <Link to="/">Home</Link>
-          </span>
-          <span className="transition-colors ease-in-out font-semibold cursor-pointer hover:text-orange-400">
-            <a href="#Features">Features</a>
-          </span>
-          <span className="transition-colors ease-in-out font-semibold cursor-pointer hover:text-orange-400">
-            <a href="#JSONFormat">JSON Format</a>
-          </span>
-          <span className="transition-colors ease-in-out font-semibold cursor-pointer hover:text-orange-400">
-            <a href="#About">About</a>
-          </span>
-        </div>
-        <Link
-          className="bg-orange-400 text-md px-5 py-1.5 transition-colors ease-in-out font-bold flex items-center gap-2 rounded-md hover:cursor-pointer text-orange-800 hover:bg-orange-500"
-          to="/upload-file"
-        >
-          <FaFileImport />
-          Import
-        </Link>
       </div>
     </div>
   );
