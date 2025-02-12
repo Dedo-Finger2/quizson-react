593554f84c554a034e480695864afaaa9ee4169f
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-12 20:57:32 -0300

    Makes FeatureCard w-full

--- src/components/FeatureCard/index.tsx
+++ src/components/FeatureCard/index.tsx
@@ -6,7 +6,7 @@ type FeatureCardProps = {
 
 function FeatureCard({ title, description, icon }: FeatureCardProps) {
   return (
-    <div className="bg-orange-900/10 w-45 h-55 rounded-xl border border-orange-400/15 p-4 flex flex-col gap-4">
+    <div className="bg-orange-900/10 w-full h-full rounded-xl border border-orange-400/15 p-4 flex flex-col gap-4">
       {icon}
       <div className="flex flex-col gap-5">
         <span className="font-bold text-md">{title}</span>
