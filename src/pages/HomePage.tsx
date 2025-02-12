0beb9174e83b610047cf1a8015f4335f7d76fed6
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-12 14:56:46 -0300

    Formats HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,24 @@
-
 import Navbar from "./../components/Navbar";
 import Hero from "./../components/Hero";
 import Features from "./../components/Features";
 
 import About from "./../components/About";
 import Footer from "./../components/Footer";
+import JSONFormat from "../components/JSONFormat";
 
 function App() {
-	return (
-		<div className="mb-12">
-			<Navbar />
-			<div className="xl:200 lg:px-140 md:px-20 items-center justify-center flex flex-col gap-12">
-				<Hero />
-				<Features />
-				<About />
-				<Footer />
-			</div>
-		</div>
-	);
+  return (
+    <div className="mb-12">
+      <Navbar />
+      <div className="xl:200 lg:px-140 md:px-20 items-center justify-center flex flex-col gap-12">
+        <Hero />
+        <Features />
+        <JSONFormat />
+        <About />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
