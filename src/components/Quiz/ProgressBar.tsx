3d6529b9bc01e06b554daac138d99e570864c74f
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-12 11:08:36 -0300

    Adds ProgressBar as a reusable component

---
+++ src/components/Quiz/ProgressBar.tsx
@@ -0,0 +1,27 @@
+type ProgressBarProps = {
+  title: string;
+  description?: string;
+  percentage: number;
+};
+
+function ProgressBar({ title, description, percentage }: ProgressBarProps) {
+  return (
+    <div className="flex flex-col gap-4 justify-center select-none">
+      <span className="font-semibold text-lg">{title}</span>
+      <div className="relative">
+        <div
+          style={{ width: percentage + "%" }}
+          className="transition-all ease-in-out h-2 rounded-full bg-orange-400 absolute"
+        ></div>
+        <div className="w-full h-2 rounded-full bg-orange-300/30"></div>
+      </div>
+      {description ? (
+        <span className="text-sm text-orange-100/80">{description}</span>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
+
+export default ProgressBar;
