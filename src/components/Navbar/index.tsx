dabff3755c6c69b068f558b24d5f0e62c9a4c750
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-12 21:03:42 -0300

    Makes Quizson navbar logo centered

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,7 +2,7 @@ import { MdQuiz } from "react-icons/md";
 
 function Navbar() {
   return (
-    <div className="border-b-1 border-[#432B18] flex justify-between pb-5 pt-5 px-12 items-center text-white">
+    <div className="border-b-1 border-[#432B18] flex justify-center pb-5 pt-5 px-12 items-center text-white">
       <div className="flex items-center gap-4">
         <MdQuiz size={28} className="text-white" />
         <span className="text-white font-bold text-2xl">
