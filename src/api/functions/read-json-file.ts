71b31cbd37a499c5b7e0c847263dc3b80635bd21
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-13 13:43:00 -0300

    Adds read-json-file function

---
+++ src/api/functions/read-json-file.ts
@@ -0,0 +1,41 @@
+type Alternative = {
+  text: string;
+};
+
+type Question = {
+  title: string;
+  topic: string;
+  correctAnswerIndex: number;
+  alternatives: Alternative[];
+};
+
+type JSONFormat = {
+  questions: Question[];
+};
+
+function readJsonFile(file: File | null): Promise<JSONFormat> {
+  return new Promise((resolve, reject) => {
+    if (!file) return reject(new Error("no file provided"));
+
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      try {
+        const data = JSON.parse(String(e.target ? e.target.result : ""));
+
+        if (!data.questions || data.questions.length < 1)
+          throw new Error("invalid json format, missing questions");
+
+        resolve(data);
+      } catch (error) {
+        reject(error);
+      }
+    };
+
+    reader.onerror = () => reject(new Error("error reading file"));
+
+    reader.readAsText(file);
+  });
+}
+
+export default readJsonFile;
