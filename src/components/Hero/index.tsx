c69707d4a0020fcf4588b98f50770106b950fb4c
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-12 21:11:17 -0300

    Makes text in Hero component slight smaller, and so the import button there

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -7,14 +7,14 @@ function Hero() {
     <div className="relative w-full flex items-center justify-center mt-8">
       <div className="flex flex-col gap-6 items-center text-white absolute z-2">
         <div className="flex flex-col gap-3 items-center">
-          <h1 className="font-semibold text-2xl text-center">
+          <h1 className="font-semibold text-xl text-center">
             Import your{" "}
             <span className="font-bold underline underline-offset-4 text-orange-300">
               quizzes
             </span>{" "}
             in seconds
           </h1>
-          <p className="text-sm text-stone-100 text-center">
+          <p className="text-xs text-stone-100 text-center">
             Use{" "}
             <span className="font-bold underline underline-offset-4 text-orange-300">
               JSON
@@ -23,7 +23,7 @@ function Hero() {
           </p>
         </div>
         <Link
-          className="bg-orange-400 text-base px-4 py-1.5 transition-colors ease-in-out font-bold flex items-center gap-2 rounded-md hover:cursor-pointer text-orange-800 hover:bg-orange-500"
+          className="bg-orange-400 text-sm mb-2 px-2 py-1 transition-colors ease-in-out font-bold flex items-center gap-2 rounded-md hover:cursor-pointer text-orange-800 hover:bg-orange-500"
           to="/upload-file"
         >
           <FaFileImport />
