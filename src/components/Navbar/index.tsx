d6194007d29c82772f68b55dce335917785fed43
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-13 21:33:48 -0300

    Makes Navbar responsible

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,7 +2,7 @@ import { MdQuiz } from "react-icons/md";
 import { IoMenu } from "react-icons/io5";
 import { useState } from "react";
 import { Link } from "react-router";
-import { FaXmark } from "react-icons/fa6";
+import { FaFileImport, FaXmark } from "react-icons/fa6";
 
 import { FaShapes } from "react-icons/fa6";
 import { IoDocumentText } from "react-icons/io5";
@@ -13,25 +13,54 @@ function Navbar() {
   const [isCollapsed, setIsCollapsed] = useState(true);
 
   return (
-    <div className="relative border-b-1 border-[#432B18] flex flex-col gap-4 justify-center pb-5 pt-5 px-12 items-center text-white">
-      <div className="flex flex-col items-center gap-2">
+    <div className="relative border-b-1 border-[#432B18] flex flex-col gap-4 justify-center pb-5 pt-5 px-12 items-center text-white md:items-center md:flex-row md:justify-between md:gap-6">
+      <div className="flex flex-col items-center gap-2 md:flex-row">
         <MdQuiz size={28} className="text-white" />
         <span className="text-white font-bold text-2xl">
           <span className="text-orange-400">Quiz</span>
           son
         </span>
       </div>
+
+      <div className="hidden flex gap-12 items-center md:flex md:gap-6 md:text-sm">
+        <div className="flex gap-8">
+          <span className="transition-colors ease-in-out font-semibold cursor-pointer hover:text-orange-400">
+            <Link to="/">Home</Link>
+          </span>
+
+          <span className="transition-colors ease-in-out font-semibold cursor-pointer hover:text-orange-400">
+            <a href="#Features">Features</a>
+          </span>
+
+          <span className="transition-colors ease-in-out font-semibold cursor-pointer hover:text-orange-400">
+            <a href="#JSONFormat">JSON Format</a>
+          </span>
+
+          <span className="transition-colors ease-in-out font-semibold cursor-pointer hover:text-orange-400">
+            <a href="#About">About</a>
+          </span>
+        </div>
+
+        <Link
+          className="bg-orange-400 text-md px-5 py-1.5 transition-colors ease-in-out font-bold flex items-center gap-2 rounded-md hover:cursor-pointer text-orange-800 hover:bg-orange-500"
+          to="/upload-file"
+        >
+          <FaFileImport />
+          Import
+        </Link>
+      </div>
+
       {isCollapsed ? (
         <IoMenu
           onClick={() => setIsCollapsed((prev) => !prev)}
-          className="text-orange-50"
+          className="flex text-orange-50 md:hidden"
           size={24}
         />
       ) : (
         <FaXmark
           size={24}
           onClick={() => setIsCollapsed((prev) => !prev)}
-          className="text-orange-50"
+          className="flex text-orange-50 md:hidden"
         />
       )}
 
